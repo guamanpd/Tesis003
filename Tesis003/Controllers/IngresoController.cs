7c99e87bf86e9ff6762eb1dbbab198c567a6a6ab
Daniel Guaman <dmguaman@gmail.com>
2019-01-16 21:26:48 -0500

    Añadir funcionalidad Ingresar

--- Tesis003/Controllers/IngresoController.cs
+++ Tesis003/Controllers/IngresoController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tesis003.Models;
 
 namespace Tesis003.Controllers
 {
     public class IngresoController : Controller
     {
         // GET: Ingreso
+        [HttpGet]
         public ActionResult Ingreso()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Ingreso(UsuarioModel usuario)
         {
             return View();
         }
